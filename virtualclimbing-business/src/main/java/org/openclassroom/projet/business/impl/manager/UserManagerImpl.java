e36b61fbc8b63acb2e9be6e7121f356bcd661f3e
D4RT4GNaN <maxime.blaise06@gmail.com>
2019-08-07 11:57:00 +0200

    Adding password hashes

--- virtualclimbing-business/src/main/java/org/openclassroom/projet/business/impl/manager/UserManagerImpl.java
+++ virtualclimbing-business/src/main/java/org/openclassroom/projet/business/impl/manager/UserManagerImpl.java
@@ -1,5 +1,7 @@
 package org.openclassroom.projet.business.impl.manager;
 
+import java.security.GeneralSecurityException;
+import java.security.MessageDigest;
 import java.util.Set;
 
 import javax.inject.Inject;
@@ -30,11 +32,20 @@ public class UserManagerImpl extends AbstractManager implements UserManager {
 			throw new FunctionalException(e.getMessage());
 		}
 		
-		if (vUser.getPassword().equals(pPassword)) {
+		String hashedPassword = "";
+		
+		try {
+			hashedPassword = hashPassword(pPassword);
+		} catch (GeneralSecurityException e) {
+			throw new FunctionalException(resourceBundle.getString("manager.user.error.generalSecurity"));
+		}
+		
+		if (vUser.getPassword().equals(hashedPassword)) {
 			return vUser;
 		} else {
 			throw new FunctionalException(resourceBundle.getString("manager.user.error.incorrectPassword"));
 		}
+		
 	}
 	
 	@Override
@@ -48,6 +59,14 @@ public class UserManagerImpl extends AbstractManager implements UserManager {
             throw new FunctionalException(resourceBundle.getString("manager.user.error.validation"),
                                           new ConstraintViolationException(vViolations));
         } 
+        
+        String rawPassword = pUser.getPassword();
+        try {
+			String hashedPassword = hashPassword(rawPassword);
+			pUser.setPassword(hashedPassword);
+		} catch (GeneralSecurityException e) {
+			e.printStackTrace();
+		}
 		
         TransactionStatus vTransactionStatus
         = platformTransactionManager.getTransaction(new DefaultTransactionDefinition());
@@ -78,6 +97,14 @@ public class UserManagerImpl extends AbstractManager implements UserManager {
 		if (!pUser.getPassword().equals(pConfirmPassword))
 			throw new FunctionalException(resourceBundle.getString("manager.user.error.badConfirmPassword"));
 		
+		String rawPassword = pUser.getPassword();
+        try {
+			String hashedPassword = hashPassword(rawPassword);
+			pUser.setPassword(hashedPassword);
+		} catch (GeneralSecurityException e) {
+			e.printStackTrace();
+		}
+		
         TransactionStatus vTransactionStatus
         = platformTransactionManager.getTransaction(new DefaultTransactionDefinition());
 	    try {
@@ -91,5 +118,19 @@ public class UserManagerImpl extends AbstractManager implements UserManager {
 	            platformTransactionManager.rollback(vTransactionStatus);
 	        }
 	    }
+	}
+	
+	private String hashPassword(String pPassword) throws GeneralSecurityException {
+		MessageDigest vMessageDigest = MessageDigest.getInstance("SHA-256");
+		vMessageDigest.update(pPassword.getBytes());
+		
+		byte byteData[] = vMessageDigest.digest();
+		
+		StringBuffer vStringBuffer = new StringBuffer();
+		for (int i = 0; i < byteData.length; i++) {
+			vStringBuffer.append(Integer.toString((byteData[i] & 0xff) + 0x100, 16).substring(1));
+        }
+		
+		return vStringBuffer.toString();
 	}
 }
